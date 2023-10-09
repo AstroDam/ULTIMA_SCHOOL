6874a4c7dfff6eeba687a961fe312d6e34acaff8
AstroDam <103147320+AstroDam@users.noreply.github.com>
2023-10-08 22:58:02 -0300

    Update M1S4.py
    FORMATAÇÃO DO TEXTO

--- M1S4.py
+++ M1S4.py
@@ -1,27 +1,15 @@
 ''''Olá! Você foi contratado (a) como programador (a) do Ultima-Bank, o melhor banco da América Latina!
-
 O Ultima-Bank ainda é um banco novo, então alguns processos internos ainda estão sendo realizados de maneira manual, com muita papelada envolvida!
-
 Para agilizar os processos internos, você foi encarregado (a) de desenvolver um sistema de cadastro de novos clientes utilizando Python!
-
 Você entregará nesta tarefa apenas um protótipo para o cadastro de 5 clientes, e caso dê certo, expandiremos para todos os milhões de clientes do Ultima-Bank.
-
 Os dados a serem salvos são:
-
 Nome (String)
 CPF (String)
 Idade (Inteiro)
 Dicas:
-
-Você pode criar uma lista para adicionar todos os clientes;
-Você pode usar um dicionário para armazenar cada cliente;
-Você vai obter os dados pela entrada padrão (usando a função input()).
+Você pode criar uma lista para adicionar todos os clientes; pode usar um dicionário para armazenar cada cliente; Você vai obter os dados pela entrada padrão (usando a função input()).
 Extra (Opcional): Desenvolver utilizando classes.
-Saída do seu programa:
 
-Você deve mostrar a seguinte mensagem para cada cliente: print("Cliente:", cliente["Nome"], "CPF:", cliente["CPF"], "Idade:", cliente["Idade"])
-Por exemplo: Cliente: John Snow CPF: 963.125.345-78 Idade: 24
-Mostre os cinco clientes, um após o outro.'''''
 # criando a classe
 class Clientes:
     def init(self):
